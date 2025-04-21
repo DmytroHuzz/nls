e5d513fad3ccd64e1665f0863cd8f6c916b841c1
Dmytro Huz <dmyhuz@gmail.com>
2025-04-21 12:01:07 +0200

    Init tests

---
+++ tests/test_nls.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bats
+
+# Path to the script under test
+SCRIPT="$BATS_TEST_DIRNAME/../nls.zsh"
+
+@test "script refuses to execute instead of source" {
+  run zsh "$SCRIPT"
+  [ "$status" -ne 0 ]
+  [[ "$output" == *"must be sourced"* ]]
+}
+
+@test "script errors when missing token" {
+  run zsh -c "source $SCRIPT"
+  [ "$status" -ne 0 ]
+  [[ "$output" == *"Usage:"* ]]
+}
